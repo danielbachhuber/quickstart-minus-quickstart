38833959688547edd407b61bd2ec71a771f7e111
Daniel Bachhuber <d@danielbachhuber.com>
2014-10-21 13:15:56 -0700

    Hook up ze recommended plugins

--- setup.sh
+++ setup.sh
@@ -23,5 +23,17 @@ if ! wp core is-installed; then
 	wp core multisite-install --url=vip.dev --title="WordPress.com VIP" --admin_user=wordpress --admin_password=wordpress --admin_email=wordpress@wordpress.com
 fi
 
+# Install the plugin pile
+wp plugin install --activate-network mrss user-switching debug-bar debug-bar-remote-requests debug-bar-extender debug-bar-console debug-bar-slow-actions debug-bar-query-count-alert wordpress-importer rewrite-rules-inspector
+
+# Jetpack!
+if [ ! -d "$DIR/wp-content/plugins/jetpack" ]; then
+	git clone git@github.com:Automattic/jetpack.git "$DIR/wp-content/plugins/jetpack"
+	wp plugin activate jetpack --network
+else
+	cd "$DIR/wp-content/plugins/jetpack"; git pull origin master
+fi
+
+# Configure a dedicated Nginx record
 sudo cp "$DIR/config/vip-nginx-conf" "/etc/nginx/sites-enabled/0-vip"
 sudo service nginx restart
