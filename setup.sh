5454bed77610b468d4ca8a8e87906814ae4b8d6e
Daniel Bachhuber <d@danielbachhuber.com>
2014-10-21 11:14:33 -0700

    Check out the VIP plugins directory

--- setup.sh
+++ setup.sh
@@ -2,8 +2,17 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# WordPress core
 if [ ! -d "$DIR/wp" ]; then
 	svn co https://core.svn.wordpress.org/trunk wp
 else
 	cd "$DIR/wp"; svn up
+fi
+
+# VIP plugins
+if [ ! -d "$DIR/wp-content/themes/vip/plugins" ]; then
+	mkdir -p "$DIR/wp-content/themes/vip"
+	svn co https://vip-svn.wordpress.com/plugins wp-content/themes/vip/plugins
+else
+	cd "$DIR/wp-content/themes/vip/plugins"; svn up
 fi
