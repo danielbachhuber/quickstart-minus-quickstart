3252f6f5d68ad3292117c3f595bf1d872e9d9172
Daniel Bachhuber <daniel@bachhuber.co>
2018-10-08 04:08:35 -0700

    Use straight IP address instead

--- wp-config.php
+++ wp-config.php
@@ -15,7 +15,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '127.0.0.1');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
